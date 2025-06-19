1af993560db409f7ddf008deb875a062eb0696f2
ondarigidion <gidion.ondari@student.moringaschool.com>
2025-06-19 13:54:28 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+// The tests expect this exact starting value
+var cats = ["Milo", "Otis", "Garfield"];
+
+//  Destructive functions (change the original array)  //
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+//  Non‑destructive functions   //
+function appendCat(name) {
+  return [...cats, name];
+}
+
+function prependCat(name) {
+  return [name, ...cats];
+}
+
+function removeLastCat() {
+  return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+  return cats.slice(1);
+}
+
+
